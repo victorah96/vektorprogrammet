f255a5b2469a3f4f039adc2444c4fe456a9158e5
lundli <lundli@pm.me>
2019-10-23 19:18:18 +0200

    Created ReportRepository

---
+++ src/AppBundle/Entity/Repository/ReportRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace AppBundle\Entity\Repository;
+
+use AppBundle\Entity\Report;
+use \Doctrine\ORM\EntityRepository;
+
+/**
+ * Class ReportRepository
+ */
+class ReportRepository extends EntityRepository
+{
+    # TODO
+    # - get data from database, sorted by year
+}
