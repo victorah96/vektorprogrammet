34b03366bd711f4770a569cf3660e1a58d94ecbf
lundli <lundli@pm.me>
2019-10-23 18:12:57 +0200

    routing added controllerFunctions added

--- src/AppBundle/Controller/ReportController.php
+++ src/AppBundle/Controller/ReportController.php
@@ -4,8 +4,28 @@ namespace AppBundle\Controller;
 
 class ReportController extends BaseController
 {
-    public function showAction()
+    public function listAction()
     {
         return $this->render('report/reports.html.twig');
+    }
+
+    public function showAction()
+    {
+        #TODO
+    }
+
+    public function createAction()
+    {
+        #TODO
+    }
+
+    public function editAction()
+    {
+        #TODO
+    }
+
+    public function deleteAction()
+    {
+        #TODO
     }
 }
