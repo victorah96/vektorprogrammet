287713882c6da826b999cdf7bbc068e3db25ab8f
victorah96 <victoria.ahmadi@vektorprogrammet.no>
2019-11-06 19:24:47 +0100

    Updated QuickLinkController with editAction, added upload file in QuickLinkType, added quicklink_logos in config.yml.

--- src/AppBundle/Controller/QuickLinkController.php
+++ src/AppBundle/Controller/QuickLinkController.php
@@ -13,9 +13,15 @@ use AppBundle\Entity\QuickLink;
 use AppBundle\Form\Type\CreateQuickLinkType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Service\FileUploader;
+
 
 class QuickLinkController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
     public function createAction(Request $request)
     {
         $user = $this->getUser();
@@ -26,6 +32,17 @@ class QuickLinkController extends Controller
         $form->handleRequest($request);
 
         if ($form->isValid()) {
+            $quickLink->setOwner($user);
+            if ($form->get('iconUrl')) {
+                dump($form->get('iconUrl'));
+                $imgPath = $this->get(FileUploader::class)->uploadQuicklink($request);
+                $quickLink->setIconUrl($imgPath);
+            }
+            //For testing purposes:
+            $quickLink->setOrderNum(4);
+            $quickLink->setVisible(true);
+
+
             $em = $this->getDoctrine()->getManager();
             $em->persist($quickLink);
             $em->flush();
@@ -36,6 +53,46 @@ class QuickLinkController extends Controller
         return $this->render('quicklinks/create_quicklinks.twig', array(
             'form' => $form->createView(),
         ));
+    }
+
+    /**
+     * @param QuickLink $quickLink
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function editAction(QuickLink $quickLink, Request $request){
+        $user = $this->getUser();
+
+        $form = $this->createForm(CreateQuickLinkType::class, $quickLink);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $quickLink->setOwner($user);
+            if ($form->get('iconUrl')) {
+                $imgPath = $this->get(FileUploader::class)->uploadQuicklink($request);
+                $quickLink->setIconUrl($imgPath);
+            }
+            //For testing purposes:
+            $quickLink->setOrderNum(4);
+            $quickLink->setVisible(true);
+
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($quickLink);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('control_panel'));
+        }
+
+        return $this->render('quicklinks/create_quicklinks.twig', array(
+            'form' => $form->createView(),
+        ));
+
+    }
+
+    public function deleteAction(Request $request) {
+
     }
 
 }
--- src/AppBundle/Form/Type/CreateQuickLinkType.php
+++ src/AppBundle/Form/Type/CreateQuickLinkType.php
@@ -13,6 +13,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -30,6 +31,13 @@ class CreateQuickLinkType extends AbstractType
             'label' => 'Link:',
             'attr' => array('placeholder' => 'Fyll inn link til quicklink her'),
         ))
+            ->add('iconUrl', FileType::class, array(
+                'required' => false,
+                'error_bubbling' => true,
+                'data_class' => null,
+                'label' => 'Last opp ny logo',
+            ))
+
         ->add('save', SubmitType::class);
     }
 
